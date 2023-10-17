16cbc331a97ea34de6d0bda2b5135fd5a19c6811
pierrebouteloup <pbouteloup@b-p.fr>
2023-10-17 14:23:05 +0200

    wip

--- test-technique.js
+++ test-technique.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-const data = [];
+let data = [];
 
 const validations = ['ARC', 'PLB', 'STR', 'ENV', 'BC', 'MOX'];
 const delais = [10, 10, 10, 5, 15, 3];
@@ -18,16 +18,34 @@ for (let i = 0; i < 5e2; i++) {
   const timestamp = date + Math.floor(30 * 60 * 60 * 1000 * Math.random());
   for (let j = 0; j < validations.length; j++) {
     const prevs = shuffle(validations.slice(0, Math.max(0, j)));
+    const timeLimit = delais[j] + Math.floor(-2 + 4 * Math.random());
+    const at = new Date(timestamp + Math.floor(5 * 60 * 60 * 1000 * Math.random()));
     data.push({
       id: crypto.randomBytes(24).toString('hex'),
       documentId,
       validation: validations[j],
-      timeLimit: delais[j] + Math.floor(-2 + 4 * Math.random()),
+      timeLimit,
       previous: prevs.slice(0, prevs.length * .99 * Math.random()),
-      at: new Date(timestamp + Math.floor(5 * 60 * 60 * 1000 * Math.random())),
-      done: Math.random() < .33
+      at,
+      doneAt: Math.random() < .33
+        ? new Date(at.getTime() + Math.floor(0.8 * timeLimit * 60 * 60 * 1000 + 2 * timeLimit * 60 * 60 * 1000 * Math.random()))
+        : undefined
     });
   }
 }
 
-fs.writeFileSync('./data.json', JSON.stringify(shuffle(data).slice(0, data.length * .5), null, 2));
+data = shuffle(data).slice(0, data.length * .5);
+
+const map = new Map();
+for (const item of data) {
+  if (!map.has(item.documentId)) {
+    map.set(item.documentId, new Set());
+  }
+  map.get(item.documentId).add(item.validation);
+}
+
+for (const item of data) {
+  item.previous = item.previous.filter(val => map.get(item.documentId).has(val));
+}
+
+fs.writeFileSync('./data.json', JSON.stringify(data, null, 2));
