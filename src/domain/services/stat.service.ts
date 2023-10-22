e520f0c8f09de81b3dab7626b5b56203eff93671
Janknn <jerem.pelchat@gmail.com>
2023-10-23 00:02:56 +0200

    :hammer:visa sorting

--- src/domain/services/stat.service.ts
+++ src/domain/services/stat.service.ts
@@ -1,7 +1,27 @@
-import { IVisa, IVisaStat } from '../entities/interfaces';
+import { IVisa, IVisaStat } from "../entities/interfaces";
 
 export class StatService {
   compute(visas: IVisa[], date: Date): IVisaStat[] {
-    throw 'Not yet implemented';
+    const visaStats: IVisaStat[] = [];
+
+    for (const visa of visas) {
+      if (visa !== undefined) {
+        const visaDate = new Date(visa.at);
+        const timeLimit = visa.timeLimit * 24 * 60 * 60 * 1000;
+        const deadline = new Date(visaDate.getTime() + timeLimit);
+        const lateness = Math.floor(
+          (date.getTime() - deadline.getTime()) / (24 * 60 * 60 * 1000)
+        );
+        if (lateness > 0) {
+          visaStats.push({
+            document: visa.document,
+            validation: visa.validation,
+            lateness: lateness,
+          });
+        }
+      }
+    }
+    visaStats.sort((a, b) => b.lateness - a.lateness);
+    return visaStats;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
-import { ExampleService } from './domain/services/example.service';
-import { StatService } from './domain/services/stat.service';
-import { IVisa } from './domain/entities/interfaces';
+import { ExampleService } from "./domain/services/example.service";
+import { StatService } from "./domain/services/stat.service";
+import { IVisa } from "./domain/entities/interfaces";
 
 const PORT = 3000;
 
@@ -14,26 +14,37 @@ async function main() {
   const exampleService = new ExampleService();
   const statService = new StatService();
 
-  visaController.get('/examples', (req, res) => {
+  visaController.get("/examples", (req, res) => {
     let quantity = parseInt(req.query.quantity as string);
-    if (isNaN(quantity) || quantity < 0 || 10e3 < quantity) {
+    if (isNaN(quantity) || quantity < 0 || quantity > 1e3) {
       quantity = 5e2;
     }
-    quantity = Math.min(quantity, 10e7);
+    quantity = Math.min(quantity, 1e7);
     res.json(exampleService.generate(quantity));
   });
 
-  visaController.post('/stats', bodyParser.json({limit: '50mb'}), (req, res) => {
-    let date = new Date(req.query.date as string);
-    if (isNaN(date.getTime())) {
-      date = new Date();
+  visaController.post(
+    "/stats",
+    bodyParser.json({ limit: "50mb" }),
+    (req, res) => {
+      let date = new Date(req.query.date as string);
+      if (isNaN(date.getTime())) {
+        date = new Date();
+      }
+      if (!Array.isArray(req.body)) {
+        return res.status(400).json({ error: "Invalid data format" });
+      }
+      const visaStats = statService.compute(req.body, date);
+
+      visaStats.sort((a, b) => b.lateness - a.lateness);
+
+      res.json(visaStats);
     }
-    res.json(statService.compute(req.body as IVisa[], date));
-  });
+  );
 
-  app.use('/api/visas', visaController);
+  app.use("/api/visas", visaController);
 
-  app.listen(PORT, () => console.log(`listening on port ${PORT}, good luck !`));
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}, good luck!`));
 }
 
 main();
